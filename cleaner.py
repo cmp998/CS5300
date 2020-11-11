08562f8e1d3a1bd927bbf6b1cfdd63f1674fe8af
Connor Pautz <cmp998@mst.edu>
2020-11-11 13:43:14 -0600

    fixed author and added initial output

--- cleaner.py
+++ cleaner.py
@@ -12,23 +12,31 @@ def clean_title(df):
 
 def clean_author(df):
     #author
-    print(df['author'])
-    df['author'] = df['author'].apply(remove_etal)
+    #print(df['author'])
+    df['author'] = df['author'].str.replace(r"&", ";")
+    #df['author'] = df['author'].apply(remove_etal)
     df['author'] = df['author'].apply(name_swap)
     df['author'] = df['author'].str.replace(r"\(.*\)", "")
     df['author'] = df['author'].str.lower()
-    print(df['author'])
+    #print(df['author'])
 
 def name_swap(name):
     if name is None:
-        return
-    if name.count(',') == 1:
+        return "NA"
+    elif name.count(';') >= 1:
+        names = name.split(';')
+        for i in range(len(names)):
+            if names[i].count(',') == 1:
+                names[i] = names[i].split(',')[1].strip(' ') + " " + names[i].split(',')[0]
+        return ",".join(names)
+    elif name.count(',') == 1:
         return name.split(',')[1].strip(' ') + " " + name.split(',')[0]
     else:
         return name
 
-def remove_etal(name):
-    name = name.replace(r'\(?.*editor.*\)?', "")
+#THIS IS DONE WITH THE REPLACE (*) in author
+#def remove_etal(name):
+#    name = name.replace(r'\(?.*editor.*\)?', "")
     #name = name.replace(r'')
 
 #df['title'] = df['title'].str.replace(r'[^\w\s]','')
@@ -64,15 +72,15 @@ def binding_pull(binding: str):
 
 def clean_publisher(df: pd.DataFrame):
     df['publisher'] = df['publisher'].str.replace(r'[ \?\.,].*', "")
-    print(df['publisher'])
+    #print(df['publisher'])
 
 def clean_descr(df: pd.DataFrame):
     df['descr'] = df['descr'].str.replace(r'Bookseller Inventory.*', "")
-    print(df.iloc[17]['descr'])
+    #print(df.iloc[17]['descr'])
   
 
 if __name__ == '__main__':
-    df = pd.read_csv('CS5300/inventory.csv')
+    df = pd.read_csv('inventory.csv')
     pd.set_option('display.max_columns', None)
     #print(df.head())
     clean_title(df)
@@ -81,3 +89,7 @@ if __name__ == '__main__':
     clean_descr(df)
     clean_publisher(df)
     clean_binding(df)
+    df.to_csv('cleaned.csv', index=False)
+    
+    #making the tables and their info
+    
