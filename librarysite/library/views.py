0c8a540000c50b2d8e9198170c0fd1f64bcf6a42
Andrew Henningsen <henningsenandrew@gmail.com>
2020-12-13 15:19:12 -0600

    Added search and cleaned up the UI

--- librarysite/library/views.py
+++ librarysite/library/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.template.loader import render_to_string
 from .models import BookCopies, Authors, PhysicalCopyQualities,GeneralCopyMiscellaneous, BookInfo
 import sys, os, csv
 
@@ -11,6 +12,9 @@ def index(request):
 def library(request):
     query = BookInfo.objects.order_by('bookID')[:]
   
+    url_parameter = request.GET.get("q")
+    if url_parameter:
+        query = query.filter(title__icontains=url_parameter)
     bookList = []
     
     for bi in query:
@@ -22,10 +26,21 @@ def library(request):
             bk = {"title": bi.title, "isbn10": bi.isbn10, "isbn13": bi.isbn13, "publisher": bi.publisher, "pubdate": bi.pubDate, "author": a[0].authorName + " and others", "book_id": bi.bookID}
         else:
 
-            bk = {"title": bi.title, "isbn10": bi.isbn10, "isbn13": bi.isbn13, "publisher": bi.publisher, "pubdate": bi.pubDate, "author": "None", "book_id": bi.bookID}
+            bk = {"title": bi.title, "isbn10": bi.isbn10, "isbn13": bi.isbn13, "publisher": bi.publisher, "pubdate": bi.pubDate, "author": "No Known Authors", "book_id": bi.bookID}
         bookList.append(bk)
 
     renderThis = {"books": bookList}
+
+    if request.is_ajax():
+        html = render_to_string(
+            template_name="books.html", 
+            context={"books": bookList}
+        )
+
+        data_dict = {"html_from_view": html}
+
+        return JsonResponse(data=data_dict, safe=False)
+
     return render(request, 'library.html', renderThis)
 
 
@@ -38,24 +53,38 @@ def libraryBook(request, book_id):
     
     for bc in query:
        
-      
+        a = Authors.objects.filter(book=bc.book)[0].authorName
+        print(a)
         pcq = PhysicalCopyQualities.objects.filter(copy=bc)[0]
         gcm = GeneralCopyMiscellaneous.objects.filter(copy=bc)[0]
         
-        bk = {"title": bc.book.title, "isbn10": bc.book.isbn10, "isbn13": bc.book.isbn13, "publisher": bc.book.publisher, "pubdate": bc.book.pubDate, "binding": pcq.binding, "condition":pcq.binding, "price":pcq.price, "signed":pcq.signed, "dustjacket":pcq.dustjacket,"description":pcq.description, "about_author": gcm.about_auth, "edition":gcm.edition, "synopsys":gcm.synopsis}
+        bk = {"title": bc.book.title, "isbn10": bc.book.isbn10, "isbn13": bc.book.isbn13, "publisher": bc.book.publisher, "pubdate": bc.book.pubDate, "binding": pcq.binding, "condition":pcq.binding, "price":pcq.price, "signed":pcq.signed, "dustjacket":pcq.dustjacket,"description":pcq.description, "about_author": gcm.about_auth, "edition":gcm.edition, "synopsys":gcm.synopsis, "author": a}
         bookList.append(bk)
 
     renderThis = {"books": bookList}
-    return render(request, 'library.html', renderThis)
+    return render(request, 'librarybooks.html', renderThis)
 
 def author(request):
     query = Authors.objects.values("authorName", "authorID").order_by('authorID').distinct()
+    url_parameter = request.GET.get("q")
+    if url_parameter:
+        query = query.filter(authorName__icontains=url_parameter)
 
     authorList = []
     for author in query:
         print(author)
         authorList.append({"name": author['authorName'], "author_id": author['authorID']})
     dictionary = {"authors": authorList}
+
+    if request.is_ajax():
+        html = render_to_string(
+            template_name="authors-search.html", 
+            context=dictionary
+        )
+
+        data_dict = {"html_from_view": html}
+
+        return JsonResponse(data=data_dict, safe=False)
     return render(request, 'author.html', dictionary)
 
 
